eef9c1d776f7c1d1ed2642637968217787f36fc2
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-11 20:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name);
+  return kittens
+}
+
+var appendKitten = (name) => {
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+var destructivelyPrependKitten = (name) => {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var prependKitten = (name) => {
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop();
+  return kittens;
+}
+
+var removeLastKitten = () => {
+  kittens = kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  kittens.shift();
+  return kittens;
+}
+
+var removeFirstKitten = () => {
+  kittens = kittens.slice(1);
+  return kittens;
+}
